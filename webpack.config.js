34ccda25a0c0c05bcd2dc840e7911ea5db1b5fbc
aoba <ipbdipbd00@gmail.com>
2018-12-05 01:02:26 +0900

    Create stationery

--- webpack.config.js
+++ webpack.config.js
@@ -1,2 +1,48 @@
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  context: path.join(__dirname, '/src'),
+  entry: path.join(__dirname, '/src/js'),
+  output: {
+    path: path.join(__dirname, '/public'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: path.join(__dirname, '/public')
+  },
+  devtool: 'eval-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [ 'babel-loader' ],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            'css-loader?modules&localIdentName=[name]__[local]__[hash:base64:5]',
+          ],
+        }),
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 51200,
+              name: './images/[name].[ext]'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin('style.css')
+  ]
+}
